e5d460135c0675aee40d389bfc388f9164f8bef0
Cristiano Chiele <cchiele@gmail.com>
2021-08-28 23:32:42 -0300

    Primeiras classes da pasta tabuleiro

---
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xadrez_console.tabuleiro
+{
+    class Tabuleiro
+    {
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+        private Peca[,] pecas;
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            this.linhas = linhas;
+            this.colunas = colunas;
+
+            this.pecas = new Peca[this.linhas, this.colunas];
+
+
+        }
+    }
+}

e94884a057051e7cb838ac723513dc6147bfc826
Cristiano Chiele <cchiele@gmail.com>
2021-09-03 22:59:42 -0300

    Melhoramos a implementacao do tabuleiro na tela

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -14,7 +14,7 @@ namespace xadrez_console
 
                 tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
                 tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
-                tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2, 4));
+                tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Branca), new Posicao(2, 4));
 
                 //tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(0, 0));
 
